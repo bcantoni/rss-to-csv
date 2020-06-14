e1e6d513988211a72b193b12defb9adade850127
Brian Cantoni <brian@cantoni.org>
2020-06-14 10:13:56 -0700

    Revert "Move out utility functions"
    This reverts commit 26e26c8a24c5716eee65d1c267bbd79e929009ee.

--- RssToCsv/__init__.py
+++ RssToCsv/__init__.py
@@ -10,12 +10,34 @@ import azure.functions as func
 import csv
 import feedparser
 import io
+import json
 import logging
 import os
 import re
 import requests
 import time
-from utility import geoIP
+
+
+def geoIP(ip):
+    ''' convert IP address into location '''
+    matches = re.search(r"^(\d+\.\d+\.\d+\.\d+):", ip)
+    if matches:
+        ip = matches[1]
+        logging.info(f"geoIP called for {ip}")
+        if 'IPSTACK_ACCESS_KEY' in os.environ:
+            access_key = os.environ['IPSTACK_ACCESS_KEY']
+            url = f"http://api.ipstack.com/{ip}?access_key={access_key}&format=1"
+            req = requests.get(url)
+            if req.status_code != 200:
+                return 'Error looking up IP address'
+            logging.info(req.content.decode('utf-8'))
+            data = json.loads(req.content.decode('utf-8'))
+            msg = f"{data['city']}, {data['region_name']} ({data['country_code']})"
+            return msg
+        else:
+            return ''
+    else:
+        return 'Bad IP address given'
 
 
 def fetchRSSandOutputCSV(url):
