852633401245f11f80ea46a3fd8530f9e64c31a3
Brian Cantoni <brian@cantoni.org>
2023-09-24 11:25:29 -0700

    Switch to ipgeolocation.io as provider

--- shared_code/utilities.py
+++ shared_code/utilities.py
@@ -7,20 +7,19 @@ import requests
 
 def geoIP(ip):
     ''' convert IP address into location '''
-    return 'N/A'
     matches = re.search(r"^(\d+\.\d+\.\d+\.\d+):", ip)
     if matches:
         ip = matches[1]
         logging.info(f"geoIP called for {ip}")
-        if 'IPSTACK_ACCESS_KEY' in os.environ:
-            access_key = os.environ['IPSTACK_ACCESS_KEY']
-            url = f"http://api.ipstack.com/{ip}?access_key={access_key}&format=1"
+        if 'IPGEO_ACCESS_KEY' in os.environ:
+            access_key = os.environ['IPGEO_ACCESS_KEY']
+            url = f"https://api.ipgeolocation.io/ipgeo?apiKey={access_key}&fields=geo&ip={ip}"
             req = requests.get(url)
             if req.status_code != 200:
                 return 'Error looking up IP address'
             logging.info(req.content.decode('utf-8'))
             data = json.loads(req.content.decode('utf-8'))
-            msg = f"{data['city']}, {data['region_name']} ({data['country_code']})"
+            msg = f"{data['city']}, {data['state_prov']} ({data['country_name']})"
             return msg
         else:
             return ''
